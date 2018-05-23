6b406e56241518f003752515cc8a3fff7e3f93ff
Heath Davies <hdavies@figeas.ch>
2018-05-23 15:42:01 +0200

    Updating instrumentation key.

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -13,6 +13,7 @@ namespace HelloWorld.WebApp
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+	            .UseApplicationInsights()
                 .Build();
     }
 }
